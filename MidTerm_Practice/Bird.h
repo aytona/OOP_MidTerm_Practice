c5ea47f1b80e6de4ea5dd817369bd3389c1a429d
Christopher Aytona <christopher.aytona@hotmail.com>
2015-02-13 13:47:52 -0500

    Added deconstructors

--- MidTerm_Practice/Bird.h
+++ MidTerm_Practice/Bird.h
@@ -14,13 +14,33 @@ protected:
 	string name;
 	int passengers;
 public:
+	bird(float w, float m, float fs, float c, string sp, string n, int p) {
+		wingspan = w;
+		mass = m;
+		flightSpeed = fs;
+		ceiling = c;
+		species = sp;
+		name = n;
+		passengers = p;
+	}
 	void setPassengers (int p) {
 		passengers = p;
 	}
 };
 
 class chicken : public bird {
+private:
+	string range;
 public:
+	chicken() : bird(2.0, 1.0, 0.0, 1.0, "Gallus", "Fred", 1) {
+		cout << "Invoking first\n";
+		range = "";
+	}
+	chicken(float w, float m, float fs, float c, string sp, string n, int p) : 
+		bird(w, m, fs, c, sp, n, p) {
+			cout << "Invoking second\n";
+			range = "r";
+	}
 	int operator+(const chicken c) const {
 		return passengers + c.passengers;
 	}
--- MidTerm_Practice/Source.cpp
+++ MidTerm_Practice/Source.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 int main() {
 
-	chicken _ch1, _ch2;
+	chicken _ch1, _ch2(10.0, 5.0, 100.0, 10000.0, "Gallus Supremus", "Frederick", 10);
 	_ch1.setPassengers(1);
 	_ch2.setPassengers(2);
 	cout << left << setw(8) << setfill('*');
